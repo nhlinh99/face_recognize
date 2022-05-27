da7102d1c907bb1d315de456d237b10d50ca748f
nhlinh99 <hlinh81999@gmail.com>
2022-05-27 13:41:11 +0700

    update code

--- identify_torch.py
+++ identify_torch.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("C:/Users/linhnh/Desktop/TMT/face_recognize/detection/scrfd")
+sys.path.append("C:/Users/linhnh/Desktop/TMT/face_recognize/Pytorch_Retinaface")
 
 import cv2
 import time
@@ -11,9 +11,10 @@ import numpy as np
 import pyrealsense2 as rs
 from sklearn import preprocessing
 
-from detection.scrfd.mmdet.apis import init_detector
+from Pytorch_Retinaface.detect import inference
+from Pytorch_Retinaface.data import cfg_mnet
+from Pytorch_Retinaface.models.retinaface import RetinaFace
 from recognition.arcface_torch.backbones import get_model
-from detection.scrfd.demo.detect import inference as detect
 from recognition.arcface_torch.recognize import inference as recognize
 
 EMBEDDING_DIMENSION = 512
@@ -45,14 +46,51 @@ def load_db(db_path, use_gpu = False):
     
     return list_len, list_id, index
 
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
 
 def identification(image, model_detection, model_recognition, threshold_detect, threshold_recog):
     list_len_embedding, list_person_name, index_faiss = face_database
     result = []
-    face_info = detect(model_detection, image, threshold_detect)
-    for i in range(len(face_info)):
-        face = face_info[i][0]
-        bounding_box = face_info[i][1]
+    box_and_landmarks, face_crop = inference(model_detection, image, threshold_detect, cfg_mnet, device)
+    for i in range(len(box_and_landmarks)):
+        bounding_box = list(map(int, box_and_landmarks[i][0:4]))
+        landmark_points = list(map(int, box_and_landmarks[i][5:15]))
+        face = face_crop[i]
+        landmarks = [[landmark_points[k], landmark_points[k+1]] for k in range(0, 10, 2)]
+
         face_embeded = recognize(model_recognition, face)[0]
         xq = face_embeded.astype('float32').reshape(1, EMBEDDING_DIMENSION)
         xq = preprocessing.normalize(xq, norm='l2')
@@ -64,18 +102,24 @@ def identification(image, model_detection, model_recognition, threshold_detect, 
             sum += list_len_embedding[idx]
             if position < sum:
                 if distances[0][0] >= threshold_recog:
-                    result.append([list_person_name[idx], bounding_box, distances[0][0]])
+                    result.append([list_person_name[idx], bounding_box, landmarks, distances[0][0]])
                 else:
-                    result.append(["stranger", bounding_box, distances[0][0]])
+                    result.append(["stranger", bounding_box, landmarks, distances[0][0]])
                 break
     return result
 
 
 face_database = load_db("./models/face_database_torch.json")
 
-model_detection = init_detector("scrfd_500m_bnkps.py", "models/detection/model_detect.pth", device='cpu') # cuda:0 
+device = torch.device("cpu")
 
-# define mxnet
+# Detection
+model_detection = RetinaFace(cfg=cfg_mnet, phase = 'test')
+model_detection = load_model(model_detection, "models/detection/mobilenet0.25_Final.pth", True)
+model_detection.eval()
+model_detection = model_detection.to(device)
+
+# Recognition
 model_recognition = get_model(name='r34', fp16=False)
 model_recognition.load_state_dict(torch.load("models/recognition/backbone_r34.pth", map_location="cpu"))
 model_recognition.eval()
@@ -106,7 +150,9 @@ while True:
     for person_info in list_person_info:
         name = person_info[0]
         bounding_box = person_info[1]
-        distance = round(person_info[2], 2)
+        landmarks = person_info[2]
+        distance = round(person_info[3], 2)
+        [cv2.circle(color_image, (point[0], point[1]), 1, (255,0,0), 2) for point in landmarks]
         cv2.rectangle(color_image, (bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), (255, 0, 0), 2)
         cv2.putText(color_image, "{} {}".format(name, str(distance)), (bounding_box[0], bounding_box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
@@ -135,7 +181,9 @@ while True:
 # for person_info in list_person_info:
 #     name = person_info[0]
 #     bounding_box = person_info[1]
-#     distance = round(person_info[2], 2)
+#     landmarks = person_info[2]
+#     distance = round(person_info[3], 2)
+#     [cv2.circle(color_image, (point[0], point[1]), 1, (255,0,0), 2) for point in landmarks]
 #     cv2.rectangle(color_image, (bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), (255, 0, 0), 2)
 #     cv2.putText(color_image, "{} {}".format(name, str(distance)), (bounding_box[0], bounding_box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
